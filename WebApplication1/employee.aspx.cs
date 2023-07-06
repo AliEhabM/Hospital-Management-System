296e6eaff54099233c3cab98b6dfdb492a6a914a
Your Name <you@example.com>
2023-07-06 19:45:53 +0300

    Fixed

--- WebApplication1/employee.aspx.cs
+++ WebApplication1/employee.aspx.cs
@@ -1,3 +1,95 @@
+<<<<<<< HEAD
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+using System.Security.Cryptography;
+using System.Web.WebSockets;
+
+namespace EasyHealthcare
+{
+    public partial class employee : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=localhost;Initial Catalog=HospitalManagement;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+        }
+
+        protected void discharge_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand check = new SqlCommand("SELECT 1 FROM PATIENT WHERE pid=@disp", con);
+            check.Parameters.AddWithValue("@disp", dispID.Text);
+            check.Connection = con;
+            SqlDataReader reader = check.ExecuteReader();
+            if (reader.Read())
+            {
+                reader.Close();
+                SqlCommand dis = new SqlCommand("DELETE FROM patient WHERE pid=@disp", con);
+                dis.Parameters.AddWithValue("@disp", dispID.Text);
+                dis.ExecuteNonQuery();
+                disFail.Visible = false;
+                disSuccess.Visible = true;
+            }
+            else
+            {
+                disFail.Visible = true;
+                disSuccess.Visible = false;
+            }
+            con.Close();
+        }
+
+        protected void find1()
+        {
+            SqlCommand check_PID = new SqlCommand("SELECT COUNT(*) FROM patient WHERE pid=@IDen", con);
+            int patientExist = (int)check_PID.ExecuteScalar();
+            check_PID.Parameters.AddWithValue("@IDen", IDentry.Text);
+            SqlCommand check_DID = new SqlCommand("SELECT COUNT(*) FROM doctor WHERE did=@IDen", con);
+            check_DID.Parameters.AddWithValue("@IDen", IDentry.Text);
+            int doctorExist = (int)check_DID.ExecuteScalar();
+            if (radioList.SelectedItem.Value == "did")
+            {
+                didView.Visible = true;
+                pidView.Visible = false;
+                didView.DataBind();
+            }
+            else
+            {
+                pidView.Visible = true;
+                didView.Visible = false;
+                didView.DataBind();
+            }
+        }
+        protected void find_Click(object sender, EventArgs e)
+        {
+            find1();
+        }
+
+        protected void SqlDataSource2_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+        }
+
+        protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+        }
+
+        protected void didView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void pidView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -84,4 +176,5 @@ namespace EasyHealthcare
 
         }
     }
+>>>>>>> origin/main
 }
